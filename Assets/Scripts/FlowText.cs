0afb9bff3546f2ec31ba762f7b7b122500a33ebd
sanoki <nkc.hirokazu.sanoki@gmail.com>
2018-10-02 09:52:03 +0900

    最初のコミット

---
+++ Assets/Scripts/FlowText.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlowText : MonoBehaviour {
+    public enum FlowState
+    {
+        STATEPOSITION,
+        FLOWMOVING,
+        STOPMOVE,
+        FINISH
+    }
+    FlowState flowState;
+
+    private RectTransform rect;
+    private Text text;
+    private int wave=1;   //現在のウェーブ
+    private float speed=12f;
+
+    public bool ChangeWave { get; set; }//waveがはじまったか
+    public bool WaveFinish { get; set; }//waveがおわったか
+    public bool GameFinish { get; set; }//ゲームが終わったか
+
+    void Start() {
+        rect = GetComponent<RectTransform>();
+        text = GetComponent<Text>();
+        //初期位置に移動
+        rect.localPosition = new Vector3(550f, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        switch (flowState)
+        {
+            case FlowState.STATEPOSITION:
+                StatePosition();
+                break;
+            case FlowState.FLOWMOVING:
+                FlowMoving();
+                break;
+            case FlowState.STOPMOVE:
+                StopMove();
+                break;
+            case FlowState.FINISH:
+                Finish();
+                break;
+        }
+    }
+
+    void StatePosition()
+    {
+        //初期位置処理
+        flowState = FlowState.FLOWMOVING;
+        ChangeWave = false;
+        text.text = "WAVE" + wave.ToString();
+    }
+
+    void FlowMoving()
+    {
+        //右から左へ流れる
+        rect.localPosition -= new Vector3(Mathf.Abs(speed), 0, 0);
+        //中心付近で原則
+        if (rect.localPosition.x > -100 && rect.localPosition.x < 100)
+        {
+            speed = 5f;
+        }
+        //-550以降まで流れないように
+        else if (rect.localPosition.x < -550)
+        {
+            flowState = FlowState.STOPMOVE;
+        }
+        //基本速度
+        else
+        {
+            speed = 12f;
+        }
+    }
+
+    void StopMove()
+    {
+        //流れた後の待機処理
+        //初期位置に戻る
+        rect.localPosition = new Vector3(550f, 0, 0);
+
+        //ゲームが終わったか
+        if (GameFinish)
+        {
+            flowState = FlowState.FINISH;
+        }
+        //1Waveが終わったか
+        else if (WaveFinish)
+        {
+            flowState = FlowState.FLOWMOVING;
+            WaveFinish = false;
+            text.text = "NEXT";
+        }
+        //Waveが始まったか
+        else if (ChangeWave)
+        {
+            flowState = FlowState.STATEPOSITION;
+            wave++;
+        }
+
+    }
+
+    void Finish()
+    {
+        text.text = "FINISH";
+        rect.localPosition = new Vector3(0f, 0, 0);
+    }
+}
---
+++ Assets/sanoki/Scripts/MultipleTargetCamera.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[RequireComponent(typeof(Camera))]
+public class MultipleTargetCamera : MonoBehaviour
+{
+    public Camera cam;//使用するカメラ
+
+    public List<GameObject> targets=new List<GameObject>();//画面内に収めたいオブジェクト
+
+    public float offset;//オフセット値
+    private void Reset()
+    {
+        cam = GetComponent<Camera>();//カメラコンポーネントを取得
+    }
+
+    private void Update()
+    {
+        if (targets[0] != null)
+        {
+            for (int i = 0; i < targets.Count; i++) { RayTest(targets[i].transform.position); }
+        }
+        //targets = GameObject.FindGameObjectsWithTag("Target").ToList();
+        transform.position = Vector3.Lerp(transform.position, calcCameraPosition(), 1.0f);
+    }
+
+    //いい感じのカメラの位置を計算
+    private Vector3 calcCameraPosition()
+    {
+        //if (targets.Count== 0) return targets[0].transform.position;
+        Vector3[] targetPosArray = new Vector3[targets.Count];
+        Vector3 centerPointWorld = new Vector3();
+        Vector3 centerPointScreen = new Vector3();
+        for(int i = 0; i < targets.Count; i++)
+        {
+            targetPosArray[i] = targets[i].transform.position;
+            //Debug.DrawRay(targetPosArray[i], Vector3.up * 5,Color.red);
+            centerPointWorld += targetPosArray[i];
+        }
+        centerPointWorld /= targets.Count;
+        centerPointScreen = Camera.main.WorldToScreenPoint(centerPointWorld);
+        //Debug.DrawRay(centerPointWorld, Vector3.up);
+        float farDistans = new float();
+        for (int i = 0; i < targets.Count; i++)
+        {
+            Vector3 diff = targetPosArray[i] - centerPointWorld;
+            diff.x *= (float)Screen.height / Screen.width;
+            float newDistans = diff.magnitude;
+            if (farDistans < newDistans)
+            {
+                farDistans = newDistans;
+            }
+        }
+        float cameraLength = farDistans / Mathf.Tan(Camera.main.fieldOfView / 2 * Mathf.Deg2Rad);
+        return centerPointWorld - transform.forward * (cameraLength + offset);
+    }
+    //rayに触れたオブジェクトのメッシュを消す
+    void RayTest(Vector3 targetPos)
+    {
+        Vector3 targetCenter=new Vector3(
+            targetPos.x-transform.position.x,
+            targetPos.y-transform.position.y+3.0f,
+            targetPos.z-transform.position.z
+            );
+        Ray ray = new Ray(transform.position,targetCenter);
+        
+
+        RaycastHit hit;
+
+        float distance = Mathf.Sqrt(
+        ((transform.position.x - targetPos.x)* (transform.position.x - targetPos.x))
+        + ((transform.position.y - targetPos.y) * (transform.position.y - targetPos.y))
+        + ((transform.position.z - targetPos.z) * (transform.position.z - targetPos.z)));
+
+        Debug.DrawRay(ray.origin, ray.direction * distance, Color.red);
+
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            if (hit.collider.tag == "Objects"){
+                hit.collider.gameObject.GetComponent<MeshRenderer>().enabled = !Physics.Raycast(ray, out hit, distance);
+            }
+        }
+    }
+}
